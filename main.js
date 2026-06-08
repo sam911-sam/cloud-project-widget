6342c0ecb43f5a5acb42e7f1405dd6f234590f83
sam911-sam <sam9110835@gmail.com>
2026-06-08 11:13:27 +0530

    Remove widget definition and add console log

--- main.js
+++ main.js
@@ -1,16 +1,1 @@
-define(
-[
-    "DS/WAFData/WAFData"
-],
-function (WAFData) {
-
-    widget.addEvent("onLoad", function () {
-
-        console.log("Widget Loaded");
-
-        widget.body.innerHTML =
-            "<h3>Widget Loaded Successfully</h3>";
-
-    });
-
-});
+console.log("Hello from main.js!");
