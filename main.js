224911ffee0bb7c604fb273f171993763d3e04b9
sam911-sam <sam9110835@gmail.com>
2026-06-08 11:29:48 +0530

    Refactor widget loading logic in main.js

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 define(function () {
 
-    widget.addEvent("onLoad", function () {
+    console.log("Widget Loaded");
 
-        console.log("Widget Loaded");
-
-        widget.body.innerHTML =
-            "<h3>Widget Loaded Successfully</h3>";
-
-    });
+    document.body.innerHTML += "<h3>Widget Loaded Successfully</h3>";
 
 });
