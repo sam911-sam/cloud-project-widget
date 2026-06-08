3856ceb650def7b84c0f9ff5f598c24a95fa8a03
sam911-sam <sam9110835@gmail.com>
2026-06-08 11:52:17 +0530

    Implement project creation functionality
    Added event listener to create button to display project details.

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
 console.log("SCRIPT RUNNING");
 
-document.getElementById("content").innerHTML =
-    "<h3>Main.js Loaded Successfully</h3>";
+document
+    .getElementById("createBtn")
+    .addEventListener("click", function () {
+
+        const projectName =
+            document.getElementById("projectName").value;
+
+        const description =
+            document.getElementById("description").value;
+
+        const result =
+            document.getElementById("result");
+
+        result.style.display = "block";
+
+        result.innerHTML = `
+            <h3>Project Created</h3>
+            <p><strong>Project Name:</strong> ${projectName}</p>
+            <p><strong>Description:</strong> ${description}</p>
+        `;
+    });
