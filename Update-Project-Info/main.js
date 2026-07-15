3b5d1521273fc2f9d0531055f6b3f40496055c90
sam911-sam <sam9110835@gmail.com>
2026-07-15 15:03:47 +0530

    Update main.js

--- Update-Project-Info/main.js
+++ Update-Project-Info/main.js
@@ -1,8 +1,94 @@
-widget.addEvent("onLoad", function () {
+var projectData = [];
+var selectedProjectId = null;
+var selectedProjectCestamp = null;
+var spaceUrl = null;
+var csrfToken = null;
 
-    console.log("Widget onLoad fired");
 
-    widget.body.innerHTML =
-        "<h3>Loading Projects...</h3>";
+(function () {
 
-});
+    function waitForWidget() {
+
+        if (typeof widget === "undefined") {
+
+            console.log("Waiting for widget object...");
+
+            setTimeout(waitForWidget, 500);
+
+            return;
+        }
+
+
+        console.log("Widget object found");
+
+
+        widget.addEvent("onLoad", function () {
+
+            console.log("Project Editor Loaded");
+
+
+            widget.body.innerHTML =
+                "<h3>Loading Projects...</h3>";
+
+
+            require([
+                "DS/WAFData/WAFData",
+                "DS/i3DXCompassServices/i3DXCompassServices"
+
+            ], function (WAFData, CompassServices) {
+
+
+                window.WAFData = WAFData;
+
+
+                CompassServices.getPlatformServices({
+
+                    platformId:
+                        widget.getValue("x3dPlatformId"),
+
+
+                    onComplete:function(services){
+
+
+                        console.log(
+                            "Services:",
+                            services
+                        );
+
+
+                        spaceUrl =
+                            services["3DSpace"];
+
+
+                        getCSRF();
+
+
+                    },
+
+
+                    onFailure:function(err){
+
+                        console.log(err);
+
+                        alert(
+                            "Failed to get platform services"
+                        );
+
+                    }
+
+
+                });
+
+
+            });
+
+
+        });
+
+    }
+
+
+    waitForWidget();
+
+
+})();
